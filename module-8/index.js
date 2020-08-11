dd8174dc034240be2a48c80b2c7910e0f8aaa0ad
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-08-11 09:25:46 +0300

    Update index.js

--- module-8/index.js
+++ module-8/index.js
@@ -1,29 +1,3 @@
-// Создай галерею с возможностью клика по ее элементам и просмотра полноразмерного изображения в модальном окне. 
-// Превью результата посмотри по ссылке.
-
-// Разбей задание на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-// Открытие модального окна по клику на элементе галереи.
-// Подмена значения атрибута src элемента img.lightbox__image.
-// Закрытие модального окна по клику на кнопку button[data-action="close-modal"].
-// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того,
-// чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
-
-
-// Разметка элемента галереи
-// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе img, 
-// и указываться в href ссылки (это необходимо для доступности).
-
-// Дополнительно
-// Следующий функционал не обязателен при сдаче задания, но будет хорошей практикой по работе с событиями.
-
-// Закрытие модального окна по клику на div.lightbox__overlay.
-// Закрытие модального окна по нажатию клавиши ESC.
-// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
-
-
 import images from "./gallery-items.js";
 
 const gallery = images.map(el =>
@@ -65,11 +39,11 @@ function closeModal() {
 function openModal(event) {
   event.preventDefault();
   window.addEventListener("keyup", closeWithEscape);
-  const img = event.target.dataset.source;
-  openImage.src = img;
-  if (event.target !== event.currentTarget) {
+    if (event.target !== event.currentTarget) {
     modalContainer.classList.add("is-open");
   };
+
+  openImage.src = event.target.dataset.source;
 };
 
 const galleryInsert = ul.insertAdjacentHTML("beforeend", gallery);
